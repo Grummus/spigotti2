ba71bee088c0938ae61c8c23fd493752f40a887d
Grummus <grahamklatt@msn.com>
2020-04-10 20:26:00 -0700

    test code for serverman.sh

--- package/opt/minecraft-server/tools/spigotti/serverman.sh
+++ package/opt/minecraft-server/tools/spigotti/serverman.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 function serverman () {
+    title="Server Manager"
+    loopint="1"
+    for server in $workingdir/servers/*; do
+        if [ -d "$server" ] ; then
+            source "$server/config"
+            loopint=$($loopint + 1)
+            echo "$server loaded"
+        fi
+    done
 
+    read -p "Pausing..." pause
+    exit
+
+    dialog --backtitle "$backtitle" --title $title \
+    --menu ""
 }
